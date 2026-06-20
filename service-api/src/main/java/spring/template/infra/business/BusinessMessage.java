568e0496a9114917191c71f333c653e58c7b3a18
IzaTecbr <izapay4devs@gmail.com>
2026-06-20 12:55:41 -0300

    Update BusinessMessage.java

--- service-api/src/main/java/spring/template/infra/business/BusinessMessage.java
+++ service-api/src/main/java/spring/template/infra/business/BusinessMessage.java
@@ -38,36 +38,6 @@ public enum BusinessMessage {
     E01("01", "O campo: %s é obrigatório","Preencha o campo e tente novamente"),
 
     E99("99", "O campo %s %s","%s"),
-
-    /*E100("100", "Usuário ou senha inválida","Verifique se os campos foram digitados corretamente"),
-    E101("101", "Campo obrigatório: %s","Preencha o campo obrigatório"),
-    E102("102", "Já existe um registro com %s igual a(o) %s","O registro deve ser único"),
-    E108("108", "Senha expirada","É necessário você alterar a senha"),
-    E109("109", "Usuário bloqueado","Favor entre em contato com o administrador do sistema"),
-    E110("110", "Usuário ou Senha Inválida","Revise os dados inseridos na autenticação"),
-    E111("111", "Usuário %s","Consulta seu acesso junto ao suporte"),
-    E116("116", "O campo %s não contem o tamanho mínimo de %s caracteres","Preencha o campo com a quantidade mínima de caracteres"),
-    E117("117", "O campo %s ultrapassa o tamanho máximo de %s caracteres","Preencha o campo com a quantidade máxima de caracteres"),
-    E118("118", "O campo %s não contem o tamanho mínimo de %s e máximo de %s caracteres","Preencha o campo com a quantidade mínima e máxima de caracteres"),
-    E119("119", "O campo %s não pode ser alterado","Contacte o administrador do sistema"),
-
-    E127("127", "%s","O campo precisa atender aos requisitos de negócio"),
-    E128("128", "%s","Para maiores informações, consulte suporte"),
-
-    E135("135", "%s","O registro precisa atender aos requisitos de negócio"),
-
-    E140("140", "O valor amortizado corresponde ao valor total do pagamento","Para valor integral, realize a compensação do pagamento"),
-    E141("141", "O valor do pagamento diverge do valor informado na compensação","Ajuste os valores de desconto e acréscimo ou realize uma amortização"),
-
-    //E141("141", "Erro ao consultra dados do IP","Reporte ao suporte técnico"),
-    E199("199", "Erro na tentativa de concluir a transação de persistência %s","Reporte ao suporte técnico"),
-    E198("198", "Erro na tentativa de realizar a consulta de %s","Reporte ao suporte técnico"),
-
-    E181("181", "Brinde indisponível, aguarde mais algumas horas","Aguarde nossa próxima comemoração"),
-    E182("182", "%s","Siga as instruções"),
-
-    E900("900", "Token inválido ou expirado","Realize uma nova autenticação"),
-*/
     ;
     private final String code;
     private final String message;
